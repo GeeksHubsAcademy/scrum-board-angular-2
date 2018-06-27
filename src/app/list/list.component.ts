24582786024316833bd4ddb0783ebcc38334fc6f
Juan <jupegarnica@gmail.com>
2018-06-27 12:48:31 +0200

    list component created

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent implements OnInit {
+  @Input () data: Object;
+  constructor() { }
+
+  ngOnInit() {
+    console.log('list inicializado')
+  }
+
+}
