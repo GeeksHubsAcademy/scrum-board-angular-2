ed9aefa0bdefbb2bf5815d96395170140b896ab1
Juan <jupegarnica@gmail.com>
2018-07-05 11:02:54 +0200

    edit task text working

--- src/app/task-item/task-item.component.ts
+++ src/app/task-item/task-item.component.ts
@@ -1,30 +1,42 @@
-import { Component, OnChanges, Input } from '@angular/core';
-import {Task} from '../task.interface';
-import {DataService} from '../data.service';
-@Component({
-  selector: 'task-item',
-  templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.css']
-})
+import { Component, OnChanges, Input} from '@angular/core';
+import { Task } from '../task.interface';
+import { DataService } from '../data.service';
+@Component( {
+    selector: 'task-item',
+    templateUrl: './task-item.component.html',
+    styleUrls: [ './task-item.component.css' ]
+} )
 export class TaskItemComponent implements OnChanges {
-  @Input() data: Task;
-  private dataService;
-  constructor(dataServ: DataService) {
-    this.dataService = dataServ;
-  }
+    @Input() data: Task;
+    private dataService;
+    private editing: boolean = false;
+    constructor( dataServ: DataService ) {
+        this.dataService = dataServ;
+    }
 
-  ngOnChanges(changes) {
-    // console.log(changes)
-  }
+    ngOnChanges( changes ) {
+        // console.log(changes)
+    }
 
-  onRemoveTask(){
-    this.dataService.removeTask(this.data);
-  }
-  onCompleted() {
-    
-    
-    this.dataService.save();
-    
-  }
+    onRemoveTask() {
+        this.dataService.removeTask( this.data );
+    }
+    onCompleted() {
+
+        this.dataService.save();
+
+    }
+    startEdit(input) {
+        this.editing = true;
+        setTimeout(()=> {
+          input.focus();
+        }, 0)
+      
+        // this.editInput.nativeElement.focus();
+    }
+    finishEdit() {
+        this.editing = false;
+        this.dataService.save();
+    }
 
 }
