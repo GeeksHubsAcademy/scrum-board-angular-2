983f1386e3c87c7dd8f4ad1e4fd5c00db73ac512
Juan <jupegarnica@gmail.com>
2018-07-11 11:01:34 +0200

    list moved saved to localstorage and only can be moved from th handle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnChanges } from '@angular/core';
 import { DataService } from './data.service';
 import { List} from './list.interface';
+import { SortablejsOptions } from 'angular-sortablejs';
 
 @Component( {
     selector: 'app-root',
@@ -11,6 +12,15 @@ export class AppComponent  {
     private lists: Array<List>;
     private addListText: String;
     private dataService: DataService;
+    private sortableOptions: SortablejsOptions = {
+        group: 'listSortable',
+        animation: 150,
+        handle: '.handle' ,
+        onUpdate: (event: any) => {
+            console.log('list onUpdate');
+            this.dataService.save();
+        }
+    };
     constructor( dataServ: DataService ) {
         this.lists = dataServ.getLists();
         this.dataService = dataServ;
