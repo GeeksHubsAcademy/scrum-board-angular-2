d6ce68e55dbce0c0133c073dfa3126e06910c115
Juan <jupegarnica@gmail.com>
2018-07-11 10:18:05 +0200

    sort tasks between diferents lists

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { DataService } from '../data.service';
 import { List } from '../list.interface';
-import { Task } from '../task.interface';
+import { SortablejsOptions } from 'angular-sortablejs';
 
 @Component( {
     selector: 'list',
@@ -12,6 +12,11 @@ export class ListComponent implements OnInit {
     @Input() data: List;
     private newTaskName: string;
     private dataService: DataService;
+    
+    private sortableOptions: SortablejsOptions = {
+        group: 'sortable',
+        animation: 150
+    };
     constructor( dataServ: DataService ) {
         this.dataService = dataServ;
     }
@@ -26,11 +31,5 @@ export class ListComponent implements OnInit {
     onRemoveList() {
         this.dataService.removeList( this.data.listId );
     }
-//   onDrop(event) {
-//       let taskItem: Task = event.dropData
-//       console.log(taskItem.text, this.data.name);
-      
-//       this.dataService.moveTask(taskItem, this.data.listId);
-//     }
 
 }
